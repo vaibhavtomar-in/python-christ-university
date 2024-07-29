79ae07bc7c4a864c5da782d5bcdc52276e0dc31c
Vaibhav <vaibhavt1612003@gmail.com>
2024-07-29 20:01:38 +0530

    implimentation of LibraryManager module

--- lab2/ques4/LibraryManager.py
+++ lab2/ques4/LibraryManager.py
@@ -4,16 +4,19 @@ def addBook(bookDict, isbn, title, author, publisher, volume, year_of_publicatio
     else:
         print("book already exists")
     return bookDict
+
 def deleteBook(bookDict, isbn):
     if(isbn in bookDict.keys()):
         del bookDict[isbn]
     else:
         print("isbn doesn't exist in the database")
+
 def bookDetail(bookDict, isbn):
     if (isbn in bookDict.keys()):
         return [isbn, bookDict[isbn]["title"], bookDict[isbn]["author"], bookDict[isbn]["publisher"], bookDict[isbn]["volume"], bookDict[isbn]["year_of_publication"] ]
     else :
         print("isbn doesnt exist in the database")
+
 def getByAuthor(bookDict, author):
     result = list()
     for i in bookDict:
@@ -23,6 +26,7 @@ def getByAuthor(bookDict, author):
         return result
     else :
         print("author not present")
+
 def getByTitle(bookDict, title):
     result = list()
     for i in bookDict:
@@ -32,5 +36,6 @@ def getByTitle(bookDict, title):
         return result
     else :
         print("title not present")
+
 def checkAvailability(bookDict, isbn):
     print("book is available ") if isbn in bookDict else print("book is not available ") 
